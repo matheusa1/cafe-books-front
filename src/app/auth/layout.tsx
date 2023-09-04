2da8e5e15f1221c7e2157f8b7a46dd78b20c92c2
Matheus Andrade <2002matheus@gmail.com>
2023-09-04 15:11:38 -0300

    fix layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -8,14 +8,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex h-screen w-screen items-center justify-center bg-backgroundLight">
-        <div className="flex h-screen w-full max-w-7xl items-center justify-center lg:justify-evenly">
-          <Image
-            src={AuthImage}
-            alt="auth image"
-            className="absolute left-0 top-0 -z-10 h-screen w-screen object-cover brightness-50 lg:static lg:max-h-[70vh] lg:w-1/2 lg:max-w-lg lg:rounded-3xl"
-          />
-          <main className="p-10">{children}</main>
+      <body>
+        <div className="flex h-screen w-screen items-center justify-center bg-backgroundLight">
+          <div className="flex h-screen w-full max-w-7xl items-center justify-center lg:justify-evenly">
+            <Image
+              src={AuthImage}
+              alt="auth image"
+              className="absolute left-0 top-0 z-0 h-screen w-screen object-cover brightness-50 lg:static lg:max-h-[70vh] lg:w-1/2 lg:max-w-lg lg:rounded-3xl"
+            />
+            <main className="z-10 p-10">{children}</main>
+          </div>
         </div>
       </body>
     </html>
