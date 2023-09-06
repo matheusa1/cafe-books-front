f01792254756fbf80181ca4667eaaf00d35b09b4
Matheus Andrade <2002matheus@gmail.com>
2023-09-06 14:26:25 -0300

    header and context done

--- src/components/organism/AdminHeader/index.tsx
+++ src/components/organism/AdminHeader/index.tsx
@@ -1,11 +1,38 @@
+'use client'
+
+import { useSidebar } from '@/context/AuthSidebarContext'
+import { List, SignOut, X } from '@phosphor-icons/react'
+import { useRouter } from 'next/navigation'
 import React, { ReactElement } from 'react'
 
-const AdminHeader: React.FC = (): ReactElement => {
+const Header: React.FC = (): ReactElement => {
+  const { toggleSidebar, isSidebarOpen } = useSidebar()
+  const router = useRouter()
+
+  const userName = 'John Doe'
+
+  const onHandleSignOut = () => {
+    router.push('/')
+  }
+
   return (
-    <div className={''}>
-      <h1>AdminHeader</h1>
-    </div>
+    <header className={'flex items-center justify-between pb-3'}>
+      <div className="flex items-center gap-2 lg:gap-6">
+        <div onClick={toggleSidebar}>
+          {isSidebarOpen ? (
+            <X className="text-2xl lg:text-3xl" />
+          ) : (
+            <List className="text-2xl lg:text-3xl" />
+          )}
+        </div>
+        <h1 className="text-base font-normal sm:text-xl lg:text-2xl">{`Olá, ${userName} 👋`}</h1>
+      </div>
+      <SignOut
+        className="cursor-pointer text-2xl lg:text-3xl"
+        onClick={onHandleSignOut}
+      />
+    </header>
   )
 }
 
-export default AdminHeader
+export default Header
