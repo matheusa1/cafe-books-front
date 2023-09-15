5ba9c4acff4be5530f2d148328172561662ff50e
Heloisa Abrantes Roncato <hroncato@alunos.utfpr.edu.br>
2023-09-15 14:46:23 -0300

    checkpoint - até bg livro

--- src/components/initialPageSections/ForYou/index.tsx
+++ src/components/initialPageSections/ForYou/index.tsx
@@ -1,15 +1,23 @@
+import SecondaryTitle from '@/components/atoms/SecondaryTitle'
 import Title from '@/components/atoms/Title'
 import { ReactElement } from 'react'
 
-export const ForYou: React.FC = (): ReactElement => {
+const ForYou: React.FC = (): ReactElement => {
   return (
-    <div className="bg-smokyGray pt-16 text-smokyGrayText">
-      <div className='gap-10'>
+    <div className="flex flex-col items-center gap-4 bg-smokyGray pt-16 text-smokyGrayText">
+      <div className="flex flex-col gap-11">
         <Title
           topText={'ESPECIAIS'}
           boldText={'Separamos'}
           text={'para você'}
         ></Title>
+        <SecondaryTitle
+          mainTitle={'Box Sherlock Holmes'}
+          smallTitle={'Arthur Conan Doyle'}
+        ></SecondaryTitle>
+      </div>
+      <div className="flex w-[90%] justify-center rounded-md bg-white p-11">
+        <p>foto</p>
       </div>
     </div>
   )
