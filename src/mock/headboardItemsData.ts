bf1c38b743e52c3489be6c3c2dce85165a800819
Matheus Andrade <2002matheus@gmail.com>
2023-09-15 11:45:38 -0300

    mock data done

---
+++ src/mock/headboardItemsData.ts
@@ -0,0 +1,33 @@
+export const headboardItemsData = [
+  {
+    id: 1,
+    title: 'Entre Deuses e Monstros',
+    price: 36.5,
+    image: '/mock/images/EntreMonstrosEDeuses.png',
+  },
+  {
+    id: 2,
+    title: 'Heartstopper: Dois garotos, um encontro (vol. 1)',
+    price: 36.5,
+    image: '/mock/images/Heartstopper.png',
+  },
+  {
+    id: 3,
+    title: 'A Revolução dos Bichos',
+    price: 36.5,
+    discountPrice: 22.5,
+    image: '/mock/images/ARevolucaoDosBichos.png',
+  },
+  {
+    id: 4,
+    title: 'Neuromancer',
+    price: 36.5,
+    image: '/mock/images/Neuromancer.png',
+  },
+  {
+    id: 5,
+    title: 'O Hobbit',
+    price: 36.5,
+    image: '/mock/images/OHobbit.png',
+  },
+]
