a2ff7926c2a3770d460beedabc8c44bba81d5c62
Matheus Andrade <2002matheus@gmail.com>
2023-09-24 12:53:31 -0300

    feat: :lipstick: header base done

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+
+import ExploreHeader from '@/components/organism/ExploreHeader'
 import React, { ReactElement } from 'react'
 
 const Explore: React.FC = (): ReactElement => {
   return (
-    <div className={''}>
-      <h1>category</h1>
+    <div className={'my-20 px-5 md:my-28 md:px-10'}>
+      <div className="flex w-full max-w-7xl flex-col items-center gap-5">
+        <ExploreHeader />
+      </div>
     </div>
   )
 }
--- src/components/atoms/BackButton/index.tsx
+++ src/components/atoms/BackButton/index.tsx
@@ -3,14 +3,27 @@
 import React, { ReactElement } from 'react'
 import { CaretLeft } from '@phosphor-icons/react'
 import { useRouter } from 'next/navigation'
+import { VariantProps, tv } from 'tailwind-variants'
 
-const BackButton: React.FC = (): ReactElement => {
+const buttonStyle = tv({
+  base: 'brownButton flex items-center justify-center gap-2 rounded-lg px-4 text-white',
+  variants: {
+    height: {
+      hFull: 'h-full',
+      hBase: 'h-10',
+    },
+  },
+  defaultVariants: {
+    height: 'hBase',
+  },
+})
+
+const BackButton: React.FC<VariantProps<typeof buttonStyle>> = ({
+  height,
+}): ReactElement => {
   const router = useRouter()
   return (
-    <button
-      className="brownButton flex h-10 items-center justify-center gap-2 rounded-lg px-4 text-white"
-      onClick={() => router.back()}
-    >
+    <button className={buttonStyle({ height })} onClick={() => router.back()}>
       <CaretLeft className="text-lg" />
       Voltar
     </button>
