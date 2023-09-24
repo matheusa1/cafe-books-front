02c77a44107a012b56ce7003c5f14a3a51e12b4c
Matheus Andrade <2002matheus@gmail.com>
2023-09-24 12:37:10 -0300

    refactor: :lipstick: remake for back style

--- src/components/atoms/BackButton/index.tsx
+++ src/components/atoms/BackButton/index.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import React, { ReactElement } from 'react'
-import Button from '../Button'
 import { CaretLeft } from '@phosphor-icons/react'
 import { useRouter } from 'next/navigation'
 
-const BackButton: React.FC<{ id: string }> = ({ id }): ReactElement => {
+const BackButton: React.FC = (): ReactElement => {
   const router = useRouter()
   return (
-    <Button id={id} content="icon" onClick={() => router.back()}>
-      <CaretLeft size={24} color="white" />
-    </Button>
+    <button
+      className="brownButton flex h-10 items-center justify-center gap-2 rounded-lg px-4 text-white"
+      onClick={() => router.back()}
+    >
+      <CaretLeft className="text-lg" />
+      Voltar
+    </button>
   )
 }
 
