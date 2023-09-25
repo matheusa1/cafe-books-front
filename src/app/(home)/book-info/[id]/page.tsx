43054e620f24d4d7643de09b210496ba1552bc88
Matheus Andrade <2002matheus@gmail.com>
2023-09-25 09:07:17 -0300

    refactor: :art: Better structure

--- src/app/(home)/book-info/[id]/page.tsx
+++ src/app/(home)/book-info/[id]/page.tsx
@@ -20,54 +20,47 @@ const getBookInfo = async (isbn: string) => {
 const BookInfo: React.FC<{ params: { id: string } }> = async ({ params }) => {
   const book = await getBookInfo(params.id)
 
-  if (book)
-    return (
-      <div
-        className={'my-20 flex flex-col items-center px-5 md:my-28 md:px-10'}
-      >
-        <div className="flex w-full max-w-7xl flex-col items-center gap-5">
-          <div className="flex items-center gap-4 self-start">
-            <BackButton />
-          </div>
-          <DetailsImageAndPriceWrapper
-            image={book.image}
-            price={book.price}
-            discountPrice={
-              book.promotional_price !== null
-                ? book.promotional_price
-                : undefined
-            }
-            title={book.title}
-          />
-          <DetailsDescription description={book.description} />
-          <DetailsSpecs
-            author={book.author}
-            editor={book.publisher}
-            language={book.language}
-            pages={book.pages}
-            release={book.year}
-            isbn={book.isbn}
-          />
-        </div>
-      </div>
-    )
-  else {
-    return (
-      <div className="flex h-screen w-screen flex-col gap-4 px-5 pt-20 md:px-10 md:pt-28">
+  return (
+    <div className={'my-20 flex flex-col items-center px-5 md:my-28 md:px-10'}>
+      <div className="flex min-h-screen w-full max-w-7xl flex-col items-center gap-5">
         <div className="flex items-center gap-4 self-start">
           <BackButton />
         </div>
-        <div className="flex flex-1 items-center justify-center">
-          <Image
-            alt="not found image"
-            src={notFound}
-            className="w-1/12 min-w-[150px]"
-          />
-          <h1 className="text-xl">Livro não encontrado...</h1>
-        </div>
+        {book ? (
+          <>
+            <DetailsImageAndPriceWrapper
+              image={book.image}
+              price={book.price}
+              discountPrice={
+                book.promotional_price !== null
+                  ? book.promotional_price
+                  : undefined
+              }
+              title={book.title}
+            />
+            <DetailsDescription description={book.description} />
+            <DetailsSpecs
+              author={book.author}
+              editor={book.publisher}
+              language={book.language}
+              pages={book.pages}
+              release={book.year}
+              isbn={book.isbn}
+            />
+          </>
+        ) : (
+          <div className="flex flex-1 items-center justify-center">
+            <Image
+              alt="not found image"
+              src={notFound}
+              className="w-1/12 min-w-[150px]"
+            />
+            <h1 className="text-xl">Livro não encontrado...</h1>
+          </div>
+        )}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default BookInfo
