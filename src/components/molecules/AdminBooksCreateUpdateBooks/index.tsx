099b855969e0a2446b05a0bff69d90042c98706f
Matheus Andrade <2002matheus@gmail.com>
2023-09-26 14:03:33 -0300

    refactor: :ambulance: now images are fixed

--- src/components/molecules/AdminBooksCreateUpdateBooks/index.tsx
+++ src/components/molecules/AdminBooksCreateUpdateBooks/index.tsx
@@ -12,6 +12,9 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { toast } from 'react-toastify'
 import { createBook, updateBook, uploadImageToCloudnary } from '@/services/api'
 
+import { Dropzone, FileCard } from '@files-ui/react'
+import Image from 'next/image'
+
 const presetUpload = process.env.NEXT_PUBLIC_PRESET_UPLOAD
 
 const AdminBooksCreateUpdateBooks: React.FC<IAdminBooksCreateUpdateBooks> = ({
@@ -24,116 +27,19 @@ const AdminBooksCreateUpdateBooks: React.FC<IAdminBooksCreateUpdateBooks> = ({
     resolver: zodResolver(AdminCreateSchema),
   })
 
+  FormMethods.watch('image')
+
   const [IsbnMessage, setIsbnMessage] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  const inputsData = [
-    {
-      label: 'ISBN',
-      placeholder: '000-0-00-000000-0',
-      name: 'isbn',
-      error: FormMethods?.formState?.errors?.isbn?.message || IsbnMessage,
-      disabled: data ? true : false,
-    },
-    {
-      label: 'Titulo',
-      placeholder: 'O dia em ...',
-      name: 'title',
-      error: FormMethods?.formState?.errors?.title?.message,
-    },
-    {
-      label: 'Descrição',
-      placeholder: 'Tudo começou quando um relógio esquisito...',
-      name: 'description',
-      error: FormMethods?.formState?.errors?.description?.message,
-      type: 'textarea',
-      colspan: 2,
-    },
-    {
-      label: 'Categorias',
-      placeholder: 'Categorias',
-      name: 'category',
-      error: FormMethods?.formState?.errors?.category?.message,
-      type: 'select',
-      isMulti: true,
-      options: categoriesList,
-    },
-    {
-      label: 'Autor',
-      placeholder: 'Stan Lee, Machado de Assis, ...',
-      name: 'author',
-      error: FormMethods?.formState?.errors?.author?.message,
-    },
-    {
-      label: 'Editora',
-      placeholder: 'Panini',
-      name: 'publisher',
-      error: FormMethods?.formState?.errors?.publisher?.message,
-    },
-    {
-      label: 'Idioma',
-      placeholder: 'Português',
-      name: 'language',
-      error: FormMethods?.formState?.errors?.language?.message,
-    },
-    {
-      label: 'Idioma Original',
-      placeholder: 'Inglês',
-      name: 'country',
-      error: FormMethods?.formState?.errors?.country?.message,
-    },
-    {
-      label: 'Ano de lançamento',
-      placeholder: '1929',
-      name: 'year',
-      error: FormMethods?.formState?.errors?.year?.message,
-      type: 'number',
-      min: 0,
-      max: new Date().getFullYear(),
-    },
-    {
-      label: 'Paginas',
-      placeholder: '2',
-      name: 'pages',
-      error: FormMethods?.formState?.errors?.pages?.message,
-      type: 'number',
-      min: 0,
-    },
-
-    {
-      label: 'Preço',
-      placeholder: '31.2',
-      name: 'price',
-      error: FormMethods?.formState?.errors?.price?.message,
-      type: 'number',
-      min: 0,
-    },
-    {
-      label: 'Preço com desconto',
-      placeholder: '31.2',
-      name: 'promotional_price',
-      error: FormMethods?.formState?.errors?.promotional_price?.message,
-      type: 'number',
-      hidden: data ? false : true,
-      min: 0,
-    },
-    {
-      label: 'Estoque',
-      placeholder: '5',
-      name: 'stock',
-      error: FormMethods?.formState?.errors?.stock?.message,
-      type: 'number',
-      min: 0,
-    },
-  ]
 
   const onHandleSubmit = async (formData: AdminCreateOutput) => {
     setIsLoading(true)
     try {
       let image
+
       if (typeof formData.image !== 'string') {
         const res = await uploadImageToCloudnary(
-          formData.image[0],
+          formData.image[0].file,
           presetUpload,
         )
 
@@ -210,8 +116,6 @@ const AdminBooksCreateUpdateBooks: React.FC<IAdminBooksCreateUpdateBooks> = ({
     }
   }, [data, FormMethods])
 
-  console.log(FormMethods?.formState?.errors)
-
   return (
     <div className="flex max-h-screen p-5">
       <FormProvider {...FormMethods}>
@@ -233,58 +137,178 @@ const AdminBooksCreateUpdateBooks: React.FC<IAdminBooksCreateUpdateBooks> = ({
             </div>
           </header>
           <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-x-10">
-            {inputsData.map((input, index) => {
-              if (!input.hidden)
-                return (
-                  <div
-                    key={index}
-                    className={input.colspan && !data ? 'md:col-span-2' : ''}
-                  >
-                    {input.type === 'textarea' ? (
-                      <Form.TextArea
-                        name={input.name}
-                        label={input.label}
-                        placeholder={input.placeholder}
-                        labelDark
-                        id={input.name}
-                        errorMessage={input.error}
-                      />
-                    ) : input.type === 'select' ? (
-                      <Form.Select
-                        name={input.name}
-                        placeholder={input.placeholder}
-                        options={input.options}
-                        isMulti={input.isMulti}
-                        label={input.label}
-                        id={input.name}
-                        labelDark
-                        errorMessage={input.error}
-                        hFull
-                      />
-                    ) : (
-                      <Form.Input
-                        name={input.name}
-                        label={input.label}
-                        placeholder={input.placeholder}
-                        labelDark
-                        errorMessage={input.error}
-                        type={input?.type}
-                        disabled={input?.disabled}
-                        id={input.name}
-                        min={input?.min}
-                        max={input?.max}
-                      />
-                    )}
-                  </div>
-                )
-            })}
-            <Form.InputFile
-              name="image"
-              label="Imagem"
-              id="imagem"
+            <Form.Input
+              name="isbn"
+              label="ISBN"
+              placeholder="000-0-00-000000-0"
               labelDark
-              placeholder="Selecione ou arraste a imagem"
-              errorMessage={FormMethods?.formState?.errors?.image?.message}
+              disabled={!!data}
+              id={'isbn'}
+              errorMessage={
+                FormMethods?.formState?.errors?.isbn?.message || IsbnMessage
+              }
+            />
+            <Form.Input
+              name="title"
+              label="Titulo"
+              placeholder="O dia em ..."
+              labelDark
+              id={'title'}
+              errorMessage={FormMethods?.formState?.errors?.title?.message}
+            />
+            <div className="md:col-span-2">
+              <Form.TextArea
+                name="description"
+                label="Descrição"
+                placeholder="Tudo começou quando um relógio esquisito..."
+                labelDark
+                id={'description'}
+                errorMessage={
+                  FormMethods?.formState?.errors?.description?.message
+                }
+              />
+            </div>
+
+            <div className="col-span-1 flex flex-col gap-2 md:col-span-2">
+              <span className="text-lg font-bold">Imagem</span>
+              <Dropzone
+                onChange={(file) => FormMethods.setValue('image', file)}
+                label="Arraste e solte a imagem aqui"
+                header={false}
+                footer={false}
+                multiple={false}
+              >
+                {typeof FormMethods.getValues('image') === 'string' ? (
+                  <Image
+                    src={data?.image}
+                    alt="book image"
+                    className="w-20"
+                    width={40}
+                    height={40}
+                  />
+                ) : (
+                  <FileCard
+                    {...FormMethods.getValues('image')?.[0]}
+                    onDelete={() => FormMethods.setValue('image', null)}
+                    info
+                    preview
+                  />
+                )}
+              </Dropzone>
+              {typeof FormMethods?.formState?.errors?.image?.message ===
+                'string' && (
+                <span className="text-red-500">
+                  {FormMethods?.formState?.errors?.image?.message}
+                </span>
+              )}
+            </div>
+
+            <Form.Select
+              name="category"
+              label="Categorias"
+              placeholder="Categorias"
+              labelDark
+              id={'category'}
+              errorMessage={FormMethods?.formState?.errors?.category?.message}
+              options={categoriesList}
+              isMulti
+            />
+
+            <Form.Input
+              name="author"
+              label="Autor"
+              placeholder="Stan Lee, Machado de Assis, ..."
+              labelDark
+              id={'author'}
+              errorMessage={FormMethods?.formState?.errors?.author?.message}
+            />
+
+            <Form.Input
+              name="publisher"
+              label="Editora"
+              placeholder="Panini"
+              labelDark
+              id={'publisher'}
+              errorMessage={FormMethods?.formState?.errors?.publisher?.message}
+            />
+
+            <Form.Input
+              name="language"
+              label="Idioma"
+              placeholder="Português"
+              labelDark
+              id={'language'}
+              errorMessage={FormMethods?.formState?.errors?.language?.message}
+            />
+
+            <Form.Input
+              name="country"
+              label="Idioma Original"
+              placeholder="Inglês"
+              labelDark
+              id={'country'}
+              errorMessage={FormMethods?.formState?.errors?.country?.message}
+            />
+
+            <Form.Input
+              name="year"
+              label="Ano de lançamento"
+              placeholder="1929"
+              labelDark
+              id={'year'}
+              errorMessage={FormMethods?.formState?.errors?.year?.message}
+              type="number"
+              min={0}
+              max={new Date().getFullYear()}
+            />
+
+            <Form.Input
+              name="pages"
+              label="Paginas"
+              placeholder="2"
+              labelDark
+              id={'pages'}
+              errorMessage={FormMethods?.formState?.errors?.pages?.message}
+              type="number"
+              min={0}
+            />
+
+            <Form.Input
+              name="price"
+              label="Preço"
+              placeholder="31.2"
+              labelDark
+              step="0.01"
+              id={'price'}
+              errorMessage={FormMethods?.formState?.errors?.price?.message}
+              type="number"
+              min={0}
+            />
+            {data && (
+              <Form.Input
+                name="promotional_price"
+                label="Preço com desconto"
+                placeholder="31.2"
+                labelDark
+                step="0.01"
+                id={'promotional_price'}
+                errorMessage={
+                  FormMethods?.formState?.errors?.promotional_price?.message
+                }
+                type="number"
+                min={0}
+              />
+            )}
+
+            <Form.Input
+              name="stock"
+              label="Estoque"
+              placeholder="5"
+              labelDark
+              id={'stock'}
+              errorMessage={FormMethods?.formState?.errors?.stock?.message}
+              type="number"
+              min={0}
             />
           </div>
           <footer className="flex flex-col gap-2 md:flex-row md:gap-10">
