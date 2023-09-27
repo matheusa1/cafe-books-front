75bbb89bb2191158e64de114d77a48fad57d2af5
Matheus Andrade <2002matheus@gmail.com>
2023-09-27 20:56:31 -0300

    Inicio do layout

--- src/app/(home)/bookmarks/page.tsx
+++ src/app/(home)/bookmarks/page.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react'
 
 const Bookmarks: React.FC = (): ReactElement => {
   return (
-    <div className={''}>
+    <div className={'my-20 flex flex-col items-center px-5 md:my-28 md:px-10'}>
       <h1>bookmarks</h1>
     </div>
   )
