2387b28db51f584f75c72044e7f08dbd67f69f6e
Matheus Andrade <2002matheus@gmail.com>
2023-09-27 16:45:52 -0300

    feat: :zap: seção integrada

--- src/components/molecules/CategoriesItem/index.tsx
+++ src/components/molecules/CategoriesItem/index.tsx
@@ -1,26 +1,46 @@
+'use client'
+
 import React, { ReactElement } from 'react'
 import { ICategoriesItem } from './types'
-import Image from 'next/image'
 import Link from 'next/link'
-
+import ImageWithFallback from '@/components/atoms/ImageWithFallback'
 const CategoriesItem: React.FC<ICategoriesItem> = ({
   image,
   title,
 }): ReactElement => {
+  const [isHover, setIsHover] = React.useState<boolean>(false)
+
   return (
     <Link
-      href={'#'}
+      href={`/explore?category=${title}`}
       className={
         'flex w-32 shrink-0 flex-col gap-2 rounded-lg p-4 text-center transition-all hover:bg-pureWhite'
       }
+      onMouseEnter={(e) => {
+        e.preventDefault()
+        setIsHover(true)
+      }}
+      onMouseLeave={(e) => {
+        e.preventDefault()
+        setIsHover(false)
+      }}
     >
-      <Image
-        src={image}
-        alt="bookImage"
-        width={1920}
-        height={1080}
-        className="h-24 w-24"
-      />
+      <div className="relative h-24 w-24">
+        <div
+          className={
+            'absolute left-1/2 top-1/2 h-24 w-24 -translate-x-1/2 -translate-y-1/2 rounded-full bg-orange-700 transition-all data-[isHover=true]:h-28 data-[isHover=true]:w-28'
+          }
+          data-isHover={isHover}
+        />
+        <ImageWithFallback
+          data-isHover={isHover}
+          src={image}
+          alt="bookImage"
+          width={96}
+          height={96}
+          className="absolute left-1/2 top-1/2 h-24 w-24 -translate-x-1/2 -translate-y-1/2 object-contain transition-all duration-500 data-[isHover=true]:rotate-12"
+        />
+      </div>
       <p>{title}</p>
     </Link>
   )
--- src/components/organism/CategoriesSwiper/index.tsx
+++ src/components/organism/CategoriesSwiper/index.tsx
@@ -1,13 +1,22 @@
 import CategoriesItem from '@/components/molecules/CategoriesItem'
 import React, { ReactElement } from 'react'
-import { categoriesData } from '@/mock/categoriesData'
-const CategoriesSwiper: React.FC = (): ReactElement => {
+import { ICategoriesSwiper } from './types'
+const CategoriesSwiper: React.FC<ICategoriesSwiper> = ({
+  categories,
+}): ReactElement => {
   return (
     <div className="flex items-center justify-center">
       <div className={'flex flex-nowrap gap-4 overflow-auto'}>
-        {categoriesData.map((item, index) => (
-          <CategoriesItem key={index} title={item.name} image={item.image} />
-        ))}
+        {categories?.map(
+          (item, index) =>
+            index < 9 && (
+              <CategoriesItem
+                key={index}
+                title={item.name}
+                image={item.image_url === null ? '' : item.image_url}
+              />
+            ),
+        )}
       </div>
     </div>
   )
