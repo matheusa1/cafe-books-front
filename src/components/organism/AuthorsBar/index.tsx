d70ffce73338b12b51581699ddf680d4613ec152
Matheus Andrade <2002matheus@gmail.com>
2023-10-30 13:14:02 -0300

    ajuste final

--- src/components/organism/AuthorsBar/index.tsx
+++ src/components/organism/AuthorsBar/index.tsx
@@ -1,14 +1,24 @@
-import { ReactElement } from 'react'
 import AuthorsItem from '@/components/molecules/AuthorsItem'
-import { authorsData } from '@/mock/authorsData'
+import { getAuthor } from '@/services/api'
 
-const Authorsbar: React.FC = (): ReactElement => {
+const getAuthorsData = async () => {
+  try {
+    const response = await getAuthor()
+    return response
+  } catch (error) {
+    return undefined
+  }
+}
+
+export const revalidate = 60 * 60 * 24
+
+const Authorsbar: React.FC = async () => {
+  const authors = await getAuthorsData()
+
   return (
     <div className="flex items-center justify-center">
       <div className={'flex flex-nowrap gap-4 overflow-auto rounded-lg'}>
-        {authorsData.map((item, index) => (
-          <AuthorsItem key={index} title={item.name} image={item.image} />
-        ))}
+        {authors?.map((item, index) => <AuthorsItem key={index} title={item.name} image={item.image_url} />)}
       </div>
     </div>
   )
