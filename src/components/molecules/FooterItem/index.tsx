b6cba80cafe83e1bbaf124af5213ad829a667b76
Matheus Andrade <2002matheus@gmail.com>
2023-11-22 20:20:44 -0300

    Refactor code and update dependencies

--- src/components/molecules/FooterItem/index.tsx
+++ src/components/molecules/FooterItem/index.tsx
@@ -1,26 +1,17 @@
 import React, { ReactElement } from 'react'
 import { IFooterItem } from './types'
 
-const FooterItem: React.FC<IFooterItem> = ({
-  icon,
-  items,
-  title,
-  onHandleClick,
-}): ReactElement => {
+const FooterItem: React.FC<IFooterItem> = ({ icon: Icon, items, title, onHandleClick }): ReactElement => {
   return (
     <div className={'flex flex-col gap-4 text-center text-white'}>
       <div className="flex items-center justify-center gap-4">
-        {icon}
+        <Icon size={24} />
         <span className="font-extrabold">{title}</span>
       </div>
       <div className="flex flex-col gap-2">
         {items.map((item, index) => (
-          <div
-            key={index}
-            className="cursor-pointer"
-            onClick={() => onHandleClick(item.data, item.title)}
-          >
-            <span className="text-sm">{item.title}</span>
+          <div key={index} className="cursor-pointer" onClick={() => onHandleClick(item.name)}>
+            <span className="text-sm">{item.name}</span>
           </div>
         ))}
       </div>
--- src/components/organism/Footer/index.tsx
+++ src/components/organism/Footer/index.tsx
@@ -4,52 +4,61 @@ import Image from 'next/image'
 import React, { ReactElement } from 'react'
 
 import Logo from '@/assets/svgs/LogoTextCol.svg'
-import { FooterData } from './data'
 import FooterItem from '@/components/molecules/FooterItem'
 import { useRouter } from 'next/navigation'
 import FooterModal from '@/components/molecules/FooterModal'
+import { Book, BookOpen } from 'lucide-react'
+import { useQuery } from '@tanstack/react-query'
+import { getCategories } from '@/services/api'
 
 const Footer: React.FC = (): ReactElement => {
+  const { data } = useQuery(['categories'], async () => {
+    try {
+      const res = await getCategories()
+      return res?.map((item) => ({ name: item.name }))
+    } catch (error) {
+      return []
+    }
+  })
+
+  const termos = [
+    {
+      name: 'Politicas de vendas',
+    },
+    { name: 'Políticas de trocas' },
+    { name: 'Políticas de privacidade' },
+    { name: 'Políticas de condições de compra' },
+  ]
+
   const router = useRouter()
   const [isOpen, setIsOpen] = React.useState<boolean>(false)
   const [title, setTitle] = React.useState<string>('')
   const [content, setContent] = React.useState<string>('')
 
-  const onHandleClick = (data: string, title: string) => {
-    if (data[0] === '/') {
-      router.push(data)
-      return
-    }
-
-    setTitle(title)
-    setContent(data)
-    setIsOpen(true)
-  }
-
   return (
-    <footer
-      className={
-        'flex w-screen flex-col items-center gap-8 bg-dark px-5 pb-24 pt-10'
-      }
-    >
+    <footer className={'flex w-screen flex-col items-center gap-8 bg-dark px-5 pb-24 pt-10'}>
       <Image src={Logo} alt={'Logo'} className="w-12" />
       <div className="flex flex-col gap-8 md:flex-row md:gap-14">
-        {FooterData.map((item, index) => (
-          <FooterItem
-            key={index}
-            icon={item.icon}
-            items={item.links}
-            title={item.title}
-            onHandleClick={onHandleClick}
-          />
-        ))}
+        <FooterItem
+          icon={Book}
+          title={'Categorias'}
+          items={data || []}
+          onHandleClick={(name) => {
+            router.push(`/explore?category=${name}`)
+          }}
+        />
+        <FooterItem
+          icon={BookOpen}
+          title={'Termos de uso'}
+          items={termos || []}
+          onHandleClick={(name) => {
+            setTitle(name)
+            setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam, sed. ')
+            setIsOpen(true)
+          }}
+        />
       </div>
-      <FooterModal
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        title={title}
-        content={content}
-      />
+      <FooterModal isOpen={isOpen} setIsOpen={setIsOpen} title={title} content={content} />
     </footer>
   )
 }
